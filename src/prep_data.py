fd7133eb20aa37efed194e457635a47267aec1c7
ygong <yuangongfdu@gmail.com>
2022-04-14 18:31:23 -0400

    first commit

---
+++ src/prep_data.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time    : 4/14/22 6:22 PM
+# @Author  : Yuan Gong
+# @Affiliation  : Massachusetts Institute of Technology
+# @Email   : yuangong@mit.edu
+# @File    : prep_data.py
+
+# download and prepare the data
